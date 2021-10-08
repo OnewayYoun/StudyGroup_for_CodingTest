fa2e964a3b9dffc098a9ffd4fd5ebb7839f7a11a
OnewayYoun <onewayy91@gmail.com>
2021-10-08 18:32:24 +0900

    update 3번(윤한길).py - dp, two-pointer

--- 11주차/3번(윤한길).py
+++ 11주차/3번(윤한길).py
@@ -1,12 +1,41 @@
 import sys
-
-
 input = sys.stdin.readline
 
 
-if __name__ == '__main__':
+# 투포인터 방식
+def two_pointer():
     n = int(input())
     sequence = list(map(int, input().split()))
+
+    right = 0
+    sum = 0
+    max_num = -float('inf')
+
+    for left in range(n):
+        while sum >= 0 and right < n:
+            sum += sequence[right]
+            right += 1
+            max_num = max(max_num, sum)
+        sum -= sequence[left]
+    print(max_num)
+
+
+# dp 방식
+def sol_with_dp():
+    n = int(input())
+    sequence = list(map(int, input().split()))
+    dp = [0] * n
+    dp[0] = sequence[0]
+
+    for i in range(1, n):
+        dp[i] = max(sequence[i], dp[i-1] + sequence[i])
+
+    print(max(dp))
+
+
+if __name__ == '__main__':
+    two_pointer()
+    # sol_with_dp()
 
 
 """
